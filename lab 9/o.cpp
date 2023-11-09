85e97d399680d16794b9eaa0590ff682062460e0
Samir Maximov <maksimov.samir@bk.ru>
2023-11-09 21:09:54 +0600

    asd

--- lab 9/o.cpp
+++ lab 9/o.cpp
@@ -4,71 +4,48 @@
 
 using namespace std;
 
-int main(){
+int main() {
     int n;
     string login, password;
     cin >> n;
 
-    map <string, string> reg;
+    map<string, string> reg;
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cin >> login >> password;
-
         reg.insert(make_pair(login, password));
     }
-    
-    int m;
 
+    int m;
     cin >> m;
 
-    multimap <string, string> sign_in;
+    multimap<string, string> sign_in;
 
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++) {
         cin >> login >> password;
-
         sign_in.insert(make_pair(login, password));
     }
-    
-    for (const auto& pair : reg)
-    {
-        int login_correct = pair.first;
-        int password_correct = pair.second;
+
+    for (const auto &pair : reg) {
+        const string &login_correct = pair.first;
+        const string &password_correct = pair.second;
 
         auto it = sign_in.find(login_correct);
-        auto it2 = sign_in.find(password_correct);
 
-        if (it != sign_in.end())
-        {
-            auto it2 = it -> second;
+        if (it != sign_in.end()) {
+            const string &it2 = it->second;
 
-            if (password_correct == it2)
-            {
+            if (password_correct == it2) {
                 cout << "password correct";
+            } else {
+                cout << "password error";
             }
-             
+        } else {
+            cout << "login error";
         }
 
-        else if (it == sign_in.end()){
-            auto it2 = it -> second;
-
-            if (password_correct != it2)
-            {
-                cout << "login error";
-            }
-            
-        }
-        
-        else if(it == sign_in.end()){
-            auto it = it -> second;
-
-            if (password_correct == it2)
-            {
-                cout << "correcet password";
-            }
-            
-        }
+        cout << endl;
     }
-       
+
+    return 0;
 }
