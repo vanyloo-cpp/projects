8362486c65682efbe1a0ab659b1db0b7275c4cce
Samir Maximov <maksimov.samir@bk.ru>
2023-11-29 15:14:56 +0600

    asd

--- lab 9/m.cpp
+++ lab 9/m.cpp
@@ -1,78 +1,36 @@
 #include <iostream>
-#include <unordered_map>
-#include <string>
+#include <queue>
 #include <vector>
 
 using namespace std;
-/*
-int main(){
-    int n;
 
+int main() {
+    int n;
     cin >> n;
 
-    unordered_map<int, string> que;
+    queue<string> boysQueue;
     vector<string> res;
 
-    for (int i = 0; i < n; i++)
-    {
-        int m;
-        string name;
-        cin >> m;
-        if (m == 1)
-        {
+    for (int i = 0; i < n; ++i) {
+        int type;
+        cin >> type;
+
+        if (type == 1) {
+            string name;
             cin >> name;
-            que.insert(make_pair(m, name));
+            boysQueue.push(name);
+        } else if (type == 2) {
+            if (!boysQueue.empty()) {
+                res.push_back(boysQueue.front());
+                boysQueue.pop();
+            } else {
+                res.push_back("queue is empty");
+            }
         }
-        else{
-            que.insert(make_pair(m, to_string(i)));
-        }
-        
     }
-    
-    for (auto &pair : que)
-    {
-        auto &it = pair.first;
 
-        if (it == 1)
-        {
-            res.push_back(pair.second);
-        }
-        else{
-            res.push_back("queue is empty");
-        }
-        
+    for (const auto &it : res) {
+        cout << it << endl;
     }
-    
-    for(auto &element : res){
-        cout << element << endl;
-    }
-}
-*/
-
-int main(){
-    int n;
-
-    cin >> n;
-
-    vector<string> res;
-
-    for (int i = 0; i < n; i++)
-    {
-        int m;
-        string name;
-        cin >> m;
-        if (m == 1)
-        {
-            cin >> name;
-            res.push_back(name);
-        }
-        else{
-            res.push_back("queue is empty");
-        }
-        
-    }    
-
-    for(auto &element : res){
-        cout << element << endl;
-    }
+    return 0;
 }
